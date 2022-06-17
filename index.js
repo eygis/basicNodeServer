f8a856b9e84e13e10180ccce0e019b0b68521ed6
eygis <ahill033@yahoo.com>
2022-06-17 17:53:06 +0900

    wording change

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(port, () => {
-    console.log(`Now listening at port ${port}...`)
+    console.log(`Listening at port ${port}...`)
 })
