23aced33d01e8f79cec387ac5cf1034833971128
eygis <ahill033@yahoo.com>
2022-06-18 11:22:18 +0900

    updated to use express

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    let path = './views/';
-    switch(req.url) {
-        case '/':
-        case '/index':
-            path += 'index.html';
-            break;
-        case '/about':
-            path += 'about.html';
-            break;
-        case '/contact':
-            path += 'contact.html';
-            break;
-        default:
-            path += '404.html';
-            break;
-    }
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            console.log(err)
-            res.end()
-        }
-        else {
-            res.statusCode = 200;
-            res.end(data);
-        }
-    })
-    
+
+const indexRoutes = ['/', '/index'];
+app.get(indexRoutes, (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/about.html'));
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/contact.html'));
+})
+const errorRoutes = ['/404', '*'];
+app.get(errorRoutes, (req, res) => {
+    res.sendFile(path.join(__dirname, './views/404.html'));
 })
 
-server.listen(port, () => {
+
+app.listen(port, () => {
     console.log(`Listening at port ${port}...`)
 })
